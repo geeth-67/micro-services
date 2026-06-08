84bb193a3b9edbcd1cb438d74272e258659aaced
vishwageethanjana <geeth.me67@gmail.com>
2026-06-08 15:24:55 +0530

    create trade service files

---
+++ trade-service/src/main/java/com/gemtrading/trade_service/client/GemResponse.java
@@ -0,0 +1,19 @@
+package com.gemtrading.trade_service.client;
+
+import java.math.BigDecimal;
+
+public class GemResponse {
+
+    private Long id;
+    private String gemCode;
+    private String type;
+    private String color;
+    private BigDecimal caratWeight;
+    private String origin;
+    private String treatment;
+    private Double pricePerCarat;
+    private Integer stockQuantity;
+    private Boolean certified;
+    private String description;
+    private Boolean active;
+}
