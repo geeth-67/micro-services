5a0c89782c16492a25eb68eaecd67b67ec1bce75
vishwageethanjana <geeth.me67@gmail.com>
2026-06-22 16:01:58 +0530

    implement circuit breaker pattern using resilience4j

--- trade-service/src/main/java/com/gemtrading/trade_service/client/GemServiceClient.java
+++ trade-service/src/main/java/com/gemtrading/trade_service/client/GemServiceClient.java
@@ -1,10 +1,11 @@
 package com.gemtrading.trade_service.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestClientException;
 
 @Component
 @RequiredArgsConstructor
@@ -12,21 +13,34 @@ public class GemServiceClient {
 
     private final RestClient restClient;
 
-//    @Value("${gem.service.url}")
-//    private String gemServiceUrl;    // accessing configuration file properties
+    private static final Logger log =
+            LoggerFactory.getLogger(GemServiceClient.class);
 
     private static final String GEM_SERVICE_URL = "http://GEM-TRADING";
 
+    @CircuitBreaker(
+            name = "gem-service",
+            fallbackMethod = "getGemFallback"
+    )
     public GemResponse getGem(Long gemId) {
-        try {
-            return restClient.get()
-                    .uri(GEM_SERVICE_URL + "/api/v1/gems/{id}" + gemId)
-                    .retrieve().body(GemResponse.class);
-        }catch (RestClientException ex) {
-            System.out.println(ex.getMessage());
-        }
-        return null;
+
+        return restClient.get()
+                .uri(GEM_SERVICE_URL + "/api/v1/gems/{id}", gemId)
+                .retrieve()
+                .body(GemResponse.class);
     }
 
+    public GemResponse getGemFallback(Long gemId, Throwable throwable) {
 
+        log.warn(
+                "Fallback method called for getGem with id: {} due to: {}",
+                gemId,
+                throwable.getMessage()
+        );
+
+        GemResponse response = new GemResponse();
+        response.setId(gemId);
+
+        return response;
+    }
 }
