c49f5882ba73537024bd65067ee0d62d23578c2d
vishwageethanjana <geeth.me67@gmail.com>
2026-06-24 15:37:29 +0530

    implement config server

---
+++ gem-ui/src/app/gems/gems.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gems',
+  standalone: true,
+  imports: [],
+  templateUrl: './gems.component.html',
+  styleUrl: './gems.component.scss'
+})
+export class GemsComponent {
+
+}
