b359d0b782983eef6963e8e1249a225d63a3cfd9
vishwageethanjana <geeth.me67@gmail.com>
2026-07-01 15:32:00 +0530

    UI implemantation

--- gem-ui/src/app/gems/gems.component.ts
+++ gem-ui/src/app/gems/gems.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../service/auth.service';
+
+interface Gem {
+  id: number;
+  name: string;
+  type: string;
+  caratWeight: number;
+  pricePerCarat: number;
+}
 
 @Component({
   selector: 'app-gems',
@@ -8,5 +18,8 @@ import { Component } from '@angular/core';
   styleUrl: './gems.component.scss'
 })
 export class GemsComponent {
+   auth = inject(AuthService);
+   private http = inject(HttpClient);
 
+   gems = signal<Gem[]>([]);
 }
