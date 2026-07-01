8c7818c5028e3983b36b0cb54049036cb20f0295
vishwageethanjana <geeth.me67@gmail.com>
2026-07-01 16:04:27 +0530

    UI implemantation

--- gem-ui/src/app/app.routes.ts
+++ gem-ui/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    title: 'Home',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'gems',
+    title: 'Gem Collection',
+    loadComponent: () =>
+      import('./gems/gems.component').then((m) => m.GemsComponent),
+  },
+  {
+    path: 'login',
+    title: 'Login',
+    loadComponent: () =>
+      import('./login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
