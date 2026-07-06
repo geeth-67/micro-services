9842bc3ddea244209adc69c4b5d6aa53c34ee667
vishwageethanjana <geeth.me67@gmail.com>
2026-07-06 10:56:38 +0530

    redis cache implement

--- gem-ui/src/app/app.config.ts
+++ gem-ui/src/app/app.config.ts
@@ -1,30 +1,15 @@
-import {
-  ApplicationConfig,
-  importProvidersFrom,
-  provideZoneChangeDetection,
-} from '@angular/core';
-import {
-  provideRouter,
-  withComponentInputBinding,
-  withInMemoryScrolling,
-  withViewTransitions,
-} from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { OAuthModule } from 'angular-oauth2-oidc';
 
 import { routes } from './app.routes';
+import { OAuthModule } from 'angular-oauth2-oidc';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(
-      routes,
-      withComponentInputBinding(),
-      withViewTransitions(),
-      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }),
-    ),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
     importProvidersFrom(OAuthModule.forRoot()),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor]))
   ],
 };
